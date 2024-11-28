bd8eb29db2458b7519468168dc4ceec925265b47
Hamza Siddiqui <hamza.siddiqui4747@gmail.com>
2024-11-28 17:04:02 +0500

    .

--- src/controller.ts
+++ src/controller.ts
@@ -16,44 +16,49 @@ export function boxToggle(show: HTMLElement, hide?: HTMLElement): void {
         show.classList.remove('hidden');
     }
     else{
-        show.classList.contains('hidden') ? show.classList.remove('hidden') : show.classList.add('hidden')
+        show.classList.contains('hidden') ? show.classList.remove('hidden') : show.classList.add('hidden');
     }
-    
 }
 
-export function fetchFormData(event: Event, isEncrypt: boolean): void {
-    const form = event.target as HTMLFormElement;
-    const formData = new FormData(form);
-    let payload;
-    let apiData;
+export function fetchFormData(event: Event, isEncrypt: boolean): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const form = event.target as HTMLFormElement;
+        const formData = new FormData(form);
+        let payload;
+        let apiData;
 
-    if (isEncrypt) {
-        payload = {
-            plainText: formData.get('text'),
-            isEncrypt: isEncrypt
-        }
-    } else {
-        payload = {
-            encryptedText: formData.get('text'),
-            key: formData.get('key'),
-            isEncrypt: isEncrypt
+        if (isEncrypt) {
+            payload = {
+                plainText: formData.get('text'),
+                isEncrypt: isEncrypt
+            };
+        } else {
+            payload = {
+                encryptedText: formData.get('text'),
+                key: formData.get('key'),
+                isEncrypt: isEncrypt
+            };
         }
 
-    }
-    apiCalling(payload).then((res) => {
-        apiData = res.data;
-        handlePopup(apiData);
-        console.warn(apiData);
-    }).catch(err => {
-        console.error("Error in API controller", err);
+        apiCalling(payload)
+            .then((res) => {
+                apiData = res.data;
+                handlePopup(apiData);
+                resolve();
+            })
+            .catch((err) => {
+                console.error("Error in API controller", err);
+                reject(err);
+            });
     });
 }
+
 
 function handlePopup(data:any): void{
     const popup:HTMLElement | null = element('#p-box');
     const key:HTMLElement | null = element('#isKey')
     const keyField:HTMLElement | null = element('#p-key');
-    const text:HTMLElement | null = element('#p-text');
+    const text:HTMLElement | null = element('#p-text');    
 
     if(popup && key && keyField && text){
         if(data.secret_key){
@@ -65,8 +70,6 @@ function handlePopup(data:any): void{
             key.classList.contains('hidden') ? '' : key.classList.add('hidden');
         }
     
-        popup.style.left = '0px'
+        popup.style.display = 'block'
     }
-
-    
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { element, boxToggle } from "./controller";
 
 // HTML ELEMENTS
-const encryptRadio:HTMLElement | null = element('#e-radio');
-const decryptRadio:HTMLElement | null = element('#d-radio');
-const encryptBox:HTMLElement | null = element('#e-box');
-const decryptBox:HTMLElement | null = element('#d-box');
-const encryptForm:HTMLElement | null = element('#e-form');
-const decryptForm:HTMLElement | null = element('#d-form');
+const encryptRadio: HTMLElement | null = element('#e-radio');
+const decryptRadio: HTMLElement | null = element('#d-radio');
+const encryptBox: HTMLElement | null = element('#e-box');
+const decryptBox: HTMLElement | null = element('#d-box');
+const encryptForm = element('#e-form') as HTMLFormElement | null;
+const decryptForm = element('#d-form') as HTMLFormElement | null;
+const closeBtn: HTMLElement | null = element('#closeBtn');
+const popup: HTMLElement | null = element('#p-box');
+const encryptIcon = element('#e-icon');
+const decryptIcon = element('#d-icon');
+const encryptLoader = element('#e-loader');
+const decryptLoader = element('#d-loader');
 // TOGGLE LOGIC
 if (encryptBox && decryptBox) {
     encryptRadio?.addEventListener('change', () => boxToggle(encryptBox, decryptBox));
@@ -15,28 +21,42 @@ if (encryptBox && decryptBox) {
     console.error('One or more elements are missing');
 }
 // EVENT LISTENERS
-if(encryptForm && decryptForm){
-    encryptForm.addEventListener('submit', (event:Event):void=> {
+if (encryptForm && decryptForm && encryptLoader && encryptIcon && decryptLoader && decryptIcon) {
+    encryptForm.addEventListener('submit', async (event: Event) => {
+        boxToggle(encryptLoader, encryptIcon);
         event.preventDefault();
-        // Lazy loading
-        import('./controller')
-            .then((module) => {
-                module.fetchFormData(event, true);
-            })
-            .catch((error) => {
-                console.error('Error loading fetchFormData:', error);
-            });
+
+        try {
+            const module = await import('./controller');
+            await module.fetchFormData(event, true);
+            boxToggle(encryptIcon, encryptLoader);
+        } catch (error) {
+            console.error('Error in fetchFormData:', error);
+            boxToggle(encryptIcon, encryptLoader);
+        }
     });
 
-    decryptForm.addEventListener('submit', (event:Event):void=> {
+    decryptForm.addEventListener('submit', async (event: Event) => {
+        boxToggle(decryptLoader, decryptIcon);
         event.preventDefault();
-        // Lazy loading
-        import('./controller')
-            .then((module) => {
-                module.fetchFormData(event, false);
-            })
-            .catch((error) => {
-                console.error('Error loading fetchFormData:', error);
-            });
+
+        try {
+            const module = await import('./controller');
+            await module.fetchFormData(event, false);
+            boxToggle(decryptIcon, decryptLoader);
+
+        } catch (error) {
+            console.error('Error in fetchFormData:', error);
+            boxToggle(decryptIcon, decryptLoader);
+        }
     });
+}
+
+if (popup) {
+    closeBtn?.addEventListener('click', () => {
+        encryptForm?.reset();
+        decryptForm?.reset();
+        // boxToggle(popup);
+        popup.style.display = 'none';
+    })
 }
